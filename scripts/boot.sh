d1d9a3adfb9f067cff9f18911c976201ba54fb0c
David Radunz <david@vimtura.com>
2021-08-22 15:00:39 +0700

    Added ulimit

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -15,6 +15,11 @@ if [ ! -d './taming-transformers' ]; then
   git clone https://github.com/CompVis/taming-transformers.git
 fi
 
+if [ ! -d './clipit' ]; then
+  git clone https://github.com/mkualquiera/clipit.git
+fi
+
 chmod +x ./download_models.sh
 ./download_models.sh
 
+ulimit -n 1048576
